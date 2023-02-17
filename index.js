21dc6be1b2a7f99bf7f4e75637111f4f196ada2b
Markos Paltser <Markos.Paltser@voco.ee>
2023-02-17 15:43:02 +0200

    WIP

--- index.js
+++ index.js
@@ -17,13 +17,30 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Middleware to parse JSON
 app.use(express.json());
 
+// Handle sign-up form submission
+app.post('/users', (req, res) => {
+    const { email, password, passwordConfirmation } = req.body;
+
+    if (!email || !password || !passwordConfirmation) {
+        return res.status(400).json({ message: 'Please provide all required fields' });
+    }
+
+    if (password !== passwordConfirmation) {
+        return res.status(400).json({ message: 'Passwords do not match' });
+    }
+
+    // Code to create a new user with the provided email and password would go here
+
+    res.status(200).json({ message: 'User created successfully' });
+});
+
 // General error handler
 app.use((err, req, res, next) => {
     console.error(err.stack);
     const status = err.statusCode || 500;
     res.status(status).send(err.message);
-})
+});
 
 app.listen(port, () => {
-    console.log(`App running. Docs at http://localhost:${port}/docs`);
-})
+    console.log(`App running. Docs at http://localhost:${port}`);
+});
